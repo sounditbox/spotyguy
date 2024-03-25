a31b97d66f026c9fbd1a22a883ddded15958c1d1
Ruslan <mutygullin@yandexlyceum.ru>
2024-03-25 17:03:03 +0400

    Added new admin models

--- player/admin.py
+++ player/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Artist, Release, Song
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'monthly_listeners', 'slug')
+    list_editable = ('monthly_listeners',)
+    list_display_links = ('name', 'slug')
+
+@admin.register(Release)
+class ReleaseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Song)
+class Song(admin.ModelAdmin):
+    pass
