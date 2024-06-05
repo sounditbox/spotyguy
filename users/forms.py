3256920c1b16142c90314ca012d711750bc363f5
Ruslan <mutygullin@yandexlyceum.ru>
2024-06-05 21:39:59 +0400

    Made register possible

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.contrib.auth import get_user_model
-from django.forms import ModelForm, TextInput, PasswordInput
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, TextInput, PasswordInput, EmailInput
 
 
 class LoginForm(ModelForm):
@@ -16,4 +18,33 @@ class LoginForm(ModelForm):
         }
 
 
+class RegisterForm(ModelForm):
+    password2 = forms.CharField(widget=PasswordInput())
 
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'password2']
+        widgets = {
+            'username': TextInput(),
+            'email': EmailInput(),
+            'password': PasswordInput(),
+        }
+        labels = {
+            'username': 'Nickname',
+            'email': 'E-mail',
+            'password': 'Password',
+            'password2': 'Repeat password',
+
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise ValidationError("Passwords do not match!")
+        return cd['password2']
+
+    def clean_email(self):
+        cd = self.cleaned_data
+        if get_user_model().objects.filter(email=cd['email']).exists():
+            raise ValidationError('User with this email already exists!')
+        return cd['email']
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.contrib import auth
 from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.models import User
 
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 # Create your views here.
@@ -27,3 +28,15 @@ def login(request: HttpRequest):
 def logout(request):
     auth.logout(request)
     return HttpResponse('Log out')
+
+
+def register(request: HttpRequest):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+    return render(request, 'register.html', context={'form': form})
